6fa4918c1dfee07432aba48b28486943067c062a
Savannah C <sgcorrell@gmail.com>
2019-04-12 19:42:10 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,7 +10,7 @@ class Triangle
   def kind
     if (side1 == side2 && side2 == side3)
       :equilateral
-    elsif (side1 == side2 )
+    elsif (side1 == side2 || side2 = side3 || side1 == side3)
     
   end
   
