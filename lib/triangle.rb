e315f079476f09bb2aead7d7530885043a759d2a
Savannah C <sgcorrell@gmail.com>
2019-04-12 19:45:40 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -20,7 +20,6 @@ class Triangle
       :scalene
     elsif (side1 <= 0 || side2 <= 0 || side3 <= 0)
       raise TriangleError
-      
     elsif (side1 + side2 <= side3 || side2 + side3 <= side1 || side1 + side3 <= side2)
       raise TriangleError
     end
