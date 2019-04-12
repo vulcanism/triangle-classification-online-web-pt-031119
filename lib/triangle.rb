e00fdcc7914cfabb9802e51d974eee34b7feae23
Savannah C <sgcorrell@gmail.com>
2019-04-12 19:54:58 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -4,8 +4,12 @@ class Triangle
     @side_2 = side_2
     @side_3 = side_3
   end
+  
+  class TriangleError < StandardError
+  # triangle error code
+  end
 
-  def kind()
+  def kind
     if (@side_1 <= 0) || (@side_2 <= 0) || (@side_3 <= 0)
       raise TriangleError
     elsif (@side_1+@side_2 <= @side_3) || (@side_1+@side_3 <= @side_2) || (@side_2+@side_3 <= @side_1)
@@ -24,6 +28,4 @@ class Triangle
 
 end
 
-class TriangleError < StandardError
-  # triangle error code
-end
+
